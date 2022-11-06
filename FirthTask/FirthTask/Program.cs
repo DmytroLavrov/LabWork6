4e8949cdc865a4d7cbf050b9c486836d014b28bb
DmytroLavrov <112572226+DmytroLavrov@users.noreply.github.com>
2022-11-06 13:03:03 +0200

    Add files via upload

---
+++ FirthTask/FirthTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FirthTask
+{
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            var num = MyClass<int>.FactoryMethod();
+            Console.WriteLine($"Вид: {num} \nЗначення: {num.GetType()}");
+            Console.WriteLine();
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            var word = MyClass<double>.FactoryMethod();
+            Console.WriteLine($"Вид: {word} \nЗначення: {word.GetType()}");
+            Console.ReadKey();
+        }
+    }
+}
